89d8fd27b950097884a87ee1cc6b5b3c1a03abef
Thwipshot <ethumpert@gmail.com>
2016-06-09 13:54:06 -0500

    added the code to auto render the content for this view

--- display-view.js
+++ display-view.js
@@ -4,6 +4,20 @@ if (window.Autorender === undefined) { window.Autorender = {}; }
 
   var DisplayView = Backbone.View.extend({
 
+    initialize: function() {
+      this.listenTo(this.model, 'change', function() {
+        this.render();
+      });
+    },
+
+    render: function() {
+      var templateText = $('#comment-template').html();
+      var templateFunc = _.template(templateText);
+      var html = templateFunc({name: this.model.get('name'), title: this.model.get('title'), comment: this.model.get('comment')});
+      $('#display-view').html(html);
+      return this;
+    }
+
 
   });
 
