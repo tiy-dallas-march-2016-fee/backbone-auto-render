798f561b75a7651bc3cba00390eb8364fee5f232
Thwipshot <ethumpert@gmail.com>
2016-06-10 09:49:55 -0500

    wrote out the code to solve the assignment properly and make it working

--- display-view.js
+++ display-view.js
@@ -4,6 +4,18 @@ if (window.Autorender === undefined) { window.Autorender = {}; }
 
   var DisplayView = Backbone.View.extend({
 
+    initialize: function() {
+      this.listenTo(this.model, "change", function() {
+        this.render();
+      })
+    },
+
+    render: function() {
+      var templateText = $("#comment-template").html();
+      var templateFunction = _.template(templateText);
+      var html = templateFunction({name: this.model.get("name"), title: this.model.get("title"), comment: this.model.get("comment")});
+      $("#display-view").html(html);
+    }
   });
 
 
--- form-view.js
+++ form-view.js
@@ -4,6 +4,17 @@ if (window.Autorender === undefined) { window.Autorender = {}; }
 
   var FormView = Backbone.View.extend({
 
+    events: {
+      "keyup input" : "inputChanged",
+      "keyup textarea" : "inputChanged"
+    },
+
+    inputChanged: function() {
+      var nameInput = this.$("#name").val();
+      var titleInput = this.$("#title").val();
+      var commentInput = this.$("#comment").val();
+      this.model.set({name: nameInput, title: titleInput, comment: commentInput});
+    }
   });
 
   window.Autorender.FormView = FormView;
